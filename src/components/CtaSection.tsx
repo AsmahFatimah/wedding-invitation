5af5e0f7ebbe56717e058393b67a73595ada8522
AsmahFatimah <asmahfatima7@gmail.com>
2026-01-10 20:48:31 +0500

    added

--- src/components/CtaSection.tsx
+++ src/components/CtaSection.tsx
@@ -9,7 +9,7 @@ const CtaSection = () => {
     <div className='ctaSection'>
     <div className='bgDiv'></div>
     <div className="cta py-4 d-flex justify-content-center align-items-center flex-column">
-        <h6>A formal Invitation to follow</h6>
+        <h6 className='text-center'>A formal Invitation to follow</h6>
         <Link to="/wedding-schedule" className='btn'>see the schedule</Link>
         <SaveToCalenderBtn title={'ali and aliya wedding'} details={'you are the gift so we dont need gifts on our wedding'} location={'Lahore, Pakistan'} startDate={'20260215T100000Z'} endDate={'20260215T120000Z'}></SaveToCalenderBtn>
     </div>
