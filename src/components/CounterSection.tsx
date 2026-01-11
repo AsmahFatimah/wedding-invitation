7914b4cc7ad478070949fcf9f3abac99c22ab36a
Asmah Fatimah <asmahfatima7@gmail.com>
2026-01-11 22:45:51 +0500

    Update CounterSection.tsx

--- src/components/CounterSection.tsx
+++ src/components/CounterSection.tsx
@@ -14,7 +14,7 @@ const CounterSection = () => {
     <div className='text-center'>
         <h2>Join us to celebrate the start of a beautiful chapter in their lives</h2>
         {/* <h6>ali and aliya</h6> */}
-         <Countdown date={new Date("2026-01-17T00:00:00").getTime()}  renderer={CountdownRenderer} />
+         <Countdown date={new Date("2026-01-30T00:00:00").getTime()}  renderer={CountdownRenderer} />
     </div>
     <div className='divider'></div>
     </div>
