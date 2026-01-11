b044cb4742dd6bdcd10488a65ee6363e4260cf46
Asmah Fatimah <asmahfatima7@gmail.com>
2026-01-12 00:31:25 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Schedule from './pages/Schedule'
-
+import ScrollToTop './components/ScrollToTop.tsx'
 function App() {
   return (
     <>
+      <ScrollToTop></ScrollToTop>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='wedding-schedule' element={<Schedule></Schedule>}></Route>
