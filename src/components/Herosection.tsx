23d449ba76583f22dd6afe7af9c67becad7866cc
Asmah Fatimah <asmahfatima7@gmail.com>
2026-01-11 22:43:29 +0500

    Update Herosection.tsx

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -2,7 +2,7 @@ const Herosection = () => {
   return (
     <>
         <div className="heroSection d-flex flex-column justify-content-center align-items-center">
-            <h1 className='text-center heading'>Safwan <br /> & <br /> Vadiha</h1>
+            <h1 className='text-center heading'>Safwan <br /> & <br /> Wadeea</h1>
             <h2 className='subHeading'>January 2026</h2>
         </div>
     </>
