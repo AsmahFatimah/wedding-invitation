47ff4b3aee7a3075d8bf8190437d91055e342226
Asmah Fatimah <asmahfatima7@gmail.com>
2026-01-12 00:23:51 +0500

    Update ScheduleCard.tsx

--- src/components/ScheduleCard.tsx
+++ src/components/ScheduleCard.tsx
@@ -1,23 +1,42 @@
-const ScheduleCard = () => {
+interface Props{
+    cardDetails:{
+            date: string,
+            time: string,
+            duration: string,
+            eventName: string,
+            venue: string
+    }[]
+
+}
+const ScheduleCard = ({ cardDetails }: Props) => {
   return (
     <>
-        <div className='container'>
+        <div className='container my-5 px-md-5 scheduleCard'>
         <h2>Schedule</h2>
     <h3>Vadiha and Safwan's Shaadi! | 19 Feb, 2:30 pm PST – 22 Feb, 11:00 pm PST All locations are in Lahore, Pak</h3>
-    <div>
-        <h6>Thu, 19 Feb</h6>
-        <div className="body d-flex justify-content-center align-items-center flex-row">
+    <div className="mt-5">
+      {cardDetails.map((cardDetail, index) => (
+        <div key={index} className="cardDetail mt-3 ">
+
+            <h6>{cardDetail.date}</h6>
+            <div className="border mb-4 mt-3"></div>
+        <div className="body d-flex justify-content-center align-items-center flex-row gap-3">
             <div className="date d-flex flex-column">
-                <p>7:00 pm - 12:00 am</p>
-                <p>5 hours</p>
+                <p>{cardDetail.time}</p>
+                <p>{cardDetail.duration}</p>
             </div>
 
             <div className='location d-flex flex-column'>
-                <p>shendi</p>
-                <p>Lahore</p>
+                <p>{cardDetail.eventName}</p>
+                <p>{cardDetail.venue}</p>
             </div>
         </div>
     </div>
+
+        ))}
+    </div>
+  
+
     </div>
     
     
